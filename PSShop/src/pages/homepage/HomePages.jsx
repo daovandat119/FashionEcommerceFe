cd73a8643bb7331dfbe653a71059f1a269cfa0fa
duchaunguyen204 <haundph34804@fpt.edu.vn>
2024-10-04 14:09:53 +0700

    them cac giao dien: banner,footer,support

--- PSShop/src/pages/homepage/HomePages.jsx
+++ PSShop/src/pages/homepage/HomePages.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Headers from '../../components/headers/Headers'
+import Banner from '../../components/banners/Banner'
+import Footers from '../../components/footers/Footers'
+import Support from '../../components/support/Support'
 
 const HomePages = () => {
   return (
     <div>
       <Headers/>
+      <Banner/>
+      
+      <Support/>
+      <Footers/>
     </div>
   )
 }
