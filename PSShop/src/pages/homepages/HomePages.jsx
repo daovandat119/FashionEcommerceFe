81c02bafc04e475eb278e4fd23089bbf61edb8aa
duchaunguyen204 <haundph34804@fpt.edu.vn>
2024-10-06 21:20:36 +0700

    hoan thanh giao dien trang chu

---
+++ PSShop/src/pages/homepages/HomePages.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Headers from '../../components/headers/Headers'
+import Banners from '../../components/banners/Banners'
+
+const HomePages = () => {
+  return (
+    <div>
+      <Headers/>
+      <Banners/>
+      
+    </div>
+  )
+}
+
+export default HomePages
