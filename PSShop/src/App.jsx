68819c5fc87b07540ef03d4551d51ebc6f1ef095
nghoa13240 <thanhhoa13022004@gmail.com>
2024-10-07 18:03:58 +0700

    tsanpham

--- PSShop/src/App.jsx
+++ PSShop/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <Router>
     <Routes>
       <Route  path="/" element={<HomePages/>}/>
-      <Route path="/home-1" element={<HomePages1 />} />
+      <Route path="shop-1" element={<HomePages1 />} />
       
     
     </Routes>
