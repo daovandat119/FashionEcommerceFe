08fad85d496b3c0e4faa3199fc7cc8cd8b483669
phamlong <longphamlala@gamil.com>
2024-10-09 20:35:45 +0700

    giao dien contact

---
+++ PSShop/src/pages/contact/index.jsx
@@ -0,0 +1,31 @@
+import Contact from "../../components/contact/Contact";
+import LocationMap from "../../components/contact/LocationMap";
+import Footers from "../../components/footers/Footers";
+import Headers from "../../components/headers/Headers";
+
+export default function ContactPage() {
+  return (
+    <>
+
+      <Headers />
+      <main className="page-wrapper">
+        <div className="mb-4 pb-4"></div>
+        <section className="contact-us container">
+          <div className="mw-930">
+            <h2 className="page-title">CONTACT US</h2>
+          </div>
+        </section>
+
+        <section className="google-map mb-5">
+          <h2 className="d-none">Contact US</h2>
+          <div id="map" className="google-map__wrapper">
+            <LocationMap />
+          </div>
+        </section>
+        <Contact />
+      </main>
+      <div className="mb-5 pb-xl-5"></div>
+      <Footers />
+    </>
+  );
+}
