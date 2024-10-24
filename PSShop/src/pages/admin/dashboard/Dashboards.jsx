8070ef17e7c58e78cfb9c0789d884725ffcec131
DangHoangHai <danghoanghai2907@gmail.com>
2024-10-24 16:37:47 +0700

    Template_Darhboard

--- PSShop/src/pages/admin/dashboard/Dashboards.jsx
+++ PSShop/src/pages/admin/dashboard/Dashboards.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { Typography, Card, CardBody, CardHeader } from "@material-tailwind/react";
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 import { FaDollarSign, FaUsers, FaUserPlus, FaChartLine } from 'react-icons/fa';
+import WebsiteViewChart from './WebsiteViewChart'; // Đảm bảo đường dẫn đúng
+import DailySalesChart from './DailySalesChart'; // Đảm bảo đường dẫn đúng
+import CompletedTasksChart from './CompletedTasksChart'; // Đảm bảo đường dẫn đúng
+import TransactionHistory from './TransactionHistory';
+import CreditBalance from './CreditBalance';
+
 
 const data = [
   { name: 'Jan', users: 400 },
@@ -14,100 +20,119 @@ const data = [
 
 const Dashboard = () => {
   return (
-    <div className="p-6 bg-gray-100 min-h-screen">
+    <>
+    <div className="px-4  bg-gray-100">
       <Typography variant="h4" color="blue-gray" className="mb-6 font-bold">
         Dashboard
       </Typography>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+      <div className="flex justify-between h-[150px] ">
         {/* Card Today's Money */}
-        <Card className="shadow-lg transition-transform transform hover:scale-105 bg-white rounded-lg border border-gray-200">
-          <CardHeader className="bg-blue-500 text-white flex items-center rounded-t-lg">
-            <div className="bg-white text-blue-500 rounded-full p-2 mr-2">
-              <FaDollarSign />
+        <div className="shadow-lg transition-transform transform hover:scale-105 w-[24%] bg-white rounded-lg border border-gray-200 p-4 flex flex-col justify-between">
+          <div className='flex items-center justify-between pb-3 border-b-2'>
+            <div className="flex p-3 justify-center bg-black rounded-xl">
+              <div className="text-white text-2xl">
+                <FaDollarSign />
+              </div>
             </div>
-            Today's Money
-          </CardHeader>
-          <CardBody className="text-center">
-            <Typography variant="h5" color="blue-gray" className="font-bold">
-              $53k
-            </Typography>
-            <Typography variant="body2" color="green">
-              +55% than last week
-            </Typography>
-          </CardBody>
-        </Card>
+            <div className='flex flex-col items-end'>
+              <p className="text-gray-600 font-medium">Today's Money</p>
+              <div className="text-2xl font-bold ">
+                $53k
+              </div>
+            </div>
+          </div>
+          <div className="text-green-500 font-medium flex pt-3 gap-2">
+            +55% <p className='text-black'>than last week</p>
+          </div>
+        </div>
 
         {/* Card Today's Users */}
-        <Card className="shadow-lg transition-transform transform hover:scale-105 bg-white rounded-lg border border-gray-200">
-          <CardHeader className="bg-green-500 text-white flex items-center rounded-t-lg">
-            <div className="bg-white text-green-500 rounded-full p-2 mr-2">
-              <FaUsers />
+        <div className="shadow-lg transition-transform transform hover:scale-105 w-[24%] bg-white rounded-lg border border-gray-200 p-4 flex flex-col justify-between">
+          <div className='flex items-center justify-between pb-3 border-b-2'>
+            <div className="flex p-3 justify-center bg-black rounded-xl">
+              <div className="text-white text-2xl">
+                <FaUsers />
+              </div>
             </div>
-            Today's Users
-          </CardHeader>
-          <CardBody className="text-center">
-            <Typography variant="h5" color="blue-gray" className="font-bold">
+            <div className='flex flex-col items-end'>
+              <p className="text-gray-600 font-medium">Today's Users</p>
+              <div className="text-2xl font-bold ">
               2,300
-            </Typography>
-            <Typography variant="body2" color="green">
-              +3% than last month
-            </Typography>
-          </CardBody>
-        </Card>
+              </div>
+            </div>
+          </div>
+          <div className="text-green-500 font-medium flex pt-3 gap-2">
+            +3% <p className='text-black'>than last month</p>
+          </div>
+        </div>
 
         {/* Card New Clients */}
-        <Card className="shadow-lg transition-transform transform hover:scale-105 bg-white rounded-lg border border-gray-200">
-          <CardHeader className="bg-red-500 text-white flex items-center rounded-t-lg">
-            <div className="bg-white text-red-500 rounded-full p-2 mr-2">
-              <FaUserPlus />
+        <div className="shadow-lg transition-transform transform hover:scale-105 w-[24%] bg-white rounded-lg border border-gray-200 p-4 flex flex-col justify-between">
+          <div className='flex items-center justify-between pb-3 border-b-2'>
+            <div className="flex p-3 justify-center bg-black rounded-xl">
+              <div className="text-white text-2xl">
+                <FaUserPlus />
+              </div>
             </div>
-            <div className="flex-1 text-center">New Clients</div>
-          </CardHeader>
-          <CardBody className="flex flex-col items-center justify-center text-center">
-            <Typography variant="h5" color="blue-gray" className="font-bold">
+            <div className='flex flex-col items-end'>
+              <p className="text-gray-600 font-medium">New Clients</p>
+              <div className="text-2xl font-bold ">
               3,462
-            </Typography>
-            <Typography variant="body2" color="red">
-              -2% than yesterday
-            </Typography>
-          </CardBody>
-        </Card>
-
-        {/* Card Sales */}
-        <Card className="shadow-lg transition-transform transform hover:scale-105 bg-white rounded-lg border border-gray-200">
-          <CardHeader className="bg-purple-500 text-white flex items-center rounded-t-lg">
-            <div className="bg-white text-purple-500 rounded-full p-2 mr-2">
-              <FaChartLine />
+              </div>
             </div>
-            Sales
-          </CardHeader>
-          <CardBody className="text-center">
-            <Typography variant="h5" color="blue-gray" className="font-bold">
+          </div>
+          <div className="text-red-500 font-medium flex pt-3 gap-2">
+            -2% <p className='text-black'>than yesterday</p>
+          </div>
+        </div>
+        {/* Card Sales */}
+        <div className="shadow-lg transition-transform transform hover:scale-105 w-[24%] bg-white rounded-lg border border-gray-200 p-4 flex flex-col justify-between">
+          <div className='flex items-center justify-between pb-3 border-b-2'>
+            <div className="flex p-3 justify-center bg-black rounded-xl">
+              <div className="text-white text-2xl">
+                <FaDollarSign />
+              </div>
+            </div>
+            <div className='flex flex-col items-end'>
+              <p className="text-gray-600 font-medium">Sales</p>
+              <div className="text-2xl font-bold ">
               $103,430
-            </Typography>
-            <Typography variant="body2" color="green">
-              +5% than yesterday
-            </Typography>
-          </CardBody>
-        </Card>
+              </div>
+            </div>
+          </div>
+          <div className="text-green-500 font-medium flex pt-3 gap-2">
+            +5% <p className='text-black'>than yesterday</p>
+          </div>
+        </div>
       </div>
 
       {/* Biểu đồ */}
-      <div className="mt-8">
-        <Typography variant="h5" color="blue-gray" className="mb-4">
-          User Growth
-        </Typography>
-        <LineChart width={600} height={300} data={data}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="users" stroke="#8884d8" />
-        </LineChart>
+      
+     <div className='flex justify-between gap-3 mt-4'>
+      
+      <div className='w-[50%]'>
+      <WebsiteViewChart />
       </div>
+
+      <div className='w-[50%]'>
+      <DailySalesChart/>
+      </div>
+
+     </div>
+    <div className='mt-3 w-full'> <CompletedTasksChart/></div>
+
+    <div className='flex gap-3 my-3'>
+    <div className='w-[30%]'>
+      <CreditBalance/>
     </div>
+    <div className='w-[70%]'>
+      <TransactionHistory/>
+    </div>
+    
+    </div>
+    </div>
+    </>
   );
 }
 
